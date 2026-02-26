9840fe3389176c417c94262539dd0130d611edbd
Allxnaq <alarrieta@estudiantec.cr>
2026-02-25 19:46:46 -0600

    Update top.sv

--- Laboratorio_01/Ejercicio 1/src/top.sv
+++ Laboratorio_01/Ejercicio 1/src/top.sv
@@ -1,1 +1,15 @@
+module top (
+    input  logic [15:0] SW,
+    output logic [15:0] LED
+);
 
+    // Solo usamos los primeros 4
+    twos_comp4 u0 (
+        .sw (SW[3:0]),
+        .led(LED[3:0])
+    );
+
+    // Apagamos los LEDs que no usamos
+    assign LED[15:4] = 12'b0;
+
+endmodule
