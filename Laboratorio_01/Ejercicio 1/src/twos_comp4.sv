70c11273725d9e3a41598f54aa29483a49de353f
Allxnaq <alarrieta@estudiantec.cr>
2026-02-25 19:45:40 -0600

    Update twos_comp4.sv

--- Laboratorio_01/Ejercicio 1/src/twos_comp4.sv
+++ Laboratorio_01/Ejercicio 1/src/twos_comp4.sv
@@ -1,1 +1,10 @@
-
+module twos_comp4 (
+    input  logic [3:0] sw,
+    output logic [3:0] led
+);
+    // Complemento a 2: -sw (en 4 bits) == (~sw + 1)
+    always_comb begin
+        led = (~sw) + 4'd1;
+        // Alternativa equivalente: led = -sw;
+    end
+endmodule
